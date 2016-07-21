e8bd61499ba947b04b2b44081675b846b854bba2
Mark Shuler <biggraydog@live.com>
2016-07-21 12:23:39 -0500

    adding daily work

---
+++ day-09-simple-dom-js/simple.js
@@ -0,0 +1,17 @@
+var clickMeDiv = document.querySelector('.click-me');
+
+clickMeDiv.textContent = 'Click me now!';
+
+//clickMeDiv.classList.add("toggled");
+
+function toggleClass() {
+
+  var containsClass = clickMeDiv.classList.contains('toggled');
+  console.log(containsClass);
+
+  if (containsClass === false) {
+  clickMeDiv.classList.add('toggled');
+  }
+  else clickMeDiv.classList.remove('toggled');
+}
+clickMeDiv.addEventListener('click', toggleClass);
