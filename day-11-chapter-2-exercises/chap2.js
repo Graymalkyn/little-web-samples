35074e81d6d9d67521b94df3e81ca82522eb75fb
Mark Shuler <biggraydog@live.com>
2016-07-25 20:49:27 -0500

    modified files

--- day-11-chapter-2-exercises/chap2.js
+++ day-11-chapter-2-exercises/chap2.js
@@ -1,10 +1,10 @@
-for (var number = 1; number <=30; number++) {
-  var output = "";
-if (number % 3 == 0)
-  output += "Fizz";
-if (number % 5 == 0)
-  output += "Buzz";
-if (number % 3 && 5 == 0)
-  output += "FizzBuzz"
-console.log(output || number);
+for (var number = 1; number <=100; number++) {
+    var output = "";
+  if (number % 3 == 0)
+    output += "Fizz";
+  if (number % 5 == 0)
+    output += "Buzz";
+  if (number % 3 && 5 == 0)
+    output += "FizzBuzz"
+  console.log(output || number);
 }
