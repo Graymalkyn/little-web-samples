7a45722938f20c9e9afb95fe58d5540bc749c2cf
Mark Shuler <biggraydog@live.com>
2016-07-31 12:49:11 -0500

    initial upload

---
+++ day-15-simple-to-do-list/script.js
@@ -0,0 +1,11 @@
+var theField = document.querySelector('#text-entry');
+var theButton = document.querySelector('#entry-button');
+var theList = document.querySelector('#the-list');
+
+function addItem(){
+  var listItem = document.createElement('li');
+  listItem.textContent = theField.value;
+  theList.appendChild(listItem);
+}
+
+theButton.addEventListener('click', addItem);
