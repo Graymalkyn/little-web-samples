9e4eda857920544a3d00b0888744ba938f59f0c8
Mark Shuler <biggraydog@live.com>
2016-08-02 20:04:18 -0500

    updated .js file

--- day-17-guess-the-number/script.js
+++ day-17-guess-the-number/script.js
@@ -2,35 +2,30 @@ var button = document.querySelector('#button');
 var input = document.querySelector('#input');
 var output = document.querySelector('.output');
 var count = 0;
-
 var inputValue;
-
-
-var randomNumber = Math.ceil(Math.random() * 10);
-
+var randomNumber = Math.ceil(Math.random() *10);
+ console.log(randomNumber)
 function guessFunc() {
   count++;
 
     inputValue = input.value;
 
-if (inputValue == randomNumber) {   //why does == work and === doesn't?  With === it was preventing 'success' function to work...no output.
-  output.textContent = "Arr!! Ya found me treasure!!";
-  return                           //why do I need this return? If not here, shows failure message.
+  if (inputValue == randomNumber) {  //why does == work and === doesn't?  With === it was preventing 'success' function to work...no output.
+    output.textContent = "Arrr!!  Ya got me booty!!";
+    return                           //why do I need this return? If not here, shows failure message.
   }
-
-    else {
-      if (inputValue > randomNumber) {
-      output.textContent = "Drop Anchor!!";
-      }
-
-      else if (inputValue < randomNumber) {
-      output.textContent = "Raise Sails!!";
-      }
+  else {
+    if (inputValue > randomNumber) {
+      output.textContent = "Drop Anchor";
     }
-    if (count == 4){
-    output.textContent = "Walk the plank, matey!!";
+    else if (inputValue < randomNumber) {
+      output.textContent = "Raise Sails";
+    }
   }
-};
+  if (count == 4) {
+    output.textContent = "Walk the plank, ya scurvy dog!!";
+  }
 
+};
 
 button.addEventListener('click', guessFunc);
