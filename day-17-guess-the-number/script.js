dcded2fcebc3a3ff2e922bd6522704aa4dc7d6f6
Mark Shuler <biggraydog@live.com>
2016-08-02 19:34:11 -0500

    .js file updated

--- day-17-guess-the-number/script.js
+++ day-17-guess-the-number/script.js
@@ -7,33 +7,27 @@ var inputValue;
 
 
 var randomNumber = Math.ceil(Math.random() * 10);
-console.log(randomNumber)
 
 function guessFunc() {
   count++;
-  console.log("count:",count)
 
     inputValue = input.value;
 
 if (inputValue == randomNumber) {   //why does == work and === doesn't?  With === it was preventing 'success' function to work...no output.
-       console.log("Treasure")
   output.textContent = "Arr!! Ya found me treasure!!";
   return                           //why do I need this return? If not here, shows failure message.
   }
 
     else {
       if (inputValue > randomNumber) {
-          console.log("Anchor")
       output.textContent = "Drop Anchor!!";
       }
 
       else if (inputValue < randomNumber) {
-          console.log("Sails")
       output.textContent = "Raise Sails!!";
       }
     }
-    if (count == 3){
-         console.log("max tries")
+    if (count == 4){
     output.textContent = "Walk the plank, matey!!";
   }
 };
