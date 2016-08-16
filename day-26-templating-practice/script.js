ede69654e5465796a6d325f1a4ae9e0c4521c179
Mark Shuler <biggraydog@live.com>
2016-08-15 20:41:25 -0500

    in class work

---
+++ day-26-templating-practice/script.js
@@ -0,0 +1,34 @@
+(function(context) {
+
+  function start() {
+
+    var stocks = [
+      {
+        name: 'Yahoo! Inc.',
+        symbol: 'YHOO',
+        price: 42.94
+      },
+      {
+        name: 'Apple Inc.',
+        symbol: 'AAPL',
+        price: 108.18
+      },
+      {
+        name: 'Microsoft Corporation',
+        symbol: 'MSFT',
+        price: 57.94
+      },
+      {
+        name: 'Sony Corp',
+        symbol: 'SNE',
+        price: 32.82
+      }
+    ];
+
+    //TODO: Create a template for this and show the data on the page.
+
+  }
+
+  context.start = start;
+
+})(window.Templating);
