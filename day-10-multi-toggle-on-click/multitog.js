dfd8dae4ec342a5f53ec4ffac73605f969a1e437
Mark Shuler <biggraydog@live.com>
2016-08-15 20:39:14 -0500

    updated to jQuery

--- day-10-multi-toggle-on-click/multitog.js
+++ day-10-multi-toggle-on-click/multitog.js
@@ -2,7 +2,6 @@ var oneButton = document.querySelector('.circle1');
 
 function clickOne() {
   oneButton.classList.toggle('on')
-  console.log('click1');
 
   }
   oneButton.addEventListener('click', clickOne)
@@ -13,7 +12,6 @@ var twoButton = document.querySelector('.circle2');
 
 function clickTwo() {
   twoButton.classList.toggle('on')
-  console.log('click2');
 
   }
   twoButton.addEventListener('click', clickTwo)
@@ -24,7 +22,6 @@ var threeButton = document.querySelector('.circle3');
 
 function clickThree() {
   threeButton.classList.toggle('on')
-  console.log('click3');
 
 }
   threeButton.addEventListener('click', clickThree)
@@ -34,7 +31,6 @@ function clickThree() {
 var fourButton = document.querySelector('.circle4');
 function clickFour() {
   fourButton.classList.toggle('on')
-  console.log('click4');
 }
 
 fourButton.addEventListener('click', clickFour)
@@ -44,7 +40,16 @@ fourButton.addEventListener('click', clickFour)
 var fiveButton = document.querySelector('.circle5');
 function clickFive() {
   fiveButton.classList.toggle('on')
-  console.log('click5');
 }
 
 fiveButton.addEventListener('click', clickFive)
+
+
+
+
+
+// var oneButton = $('.circle1');
+//
+// oneButton.click(function(){
+//   oneButton.toggleClass('on');
+// });
---
+++ day-10-multi-toggle-on-click/practice.js
@@ -0,0 +1,24 @@
+var oneButton = $('.circle1');
+oneButton.click(function(){
+  oneButton.toggleClass('on');
+});
+
+var twoButton = $('.circle2');
+twoButton.click(function(){
+  twoButton.toggleClass('on');
+});
+
+var threeButton = $('.circle3');
+threeButton.click(function(){
+  threeButton.toggleClass('on');
+});
+
+var fourButton = $('.circle4');
+fourButton.click(function(){
+  fourButton.toggleClass('on');
+});
+
+var fiveButton = $('.circle5');
+fiveButton.click(function(){
+  fiveButton.toggleClass('on');
+})
