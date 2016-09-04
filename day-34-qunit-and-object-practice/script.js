316424d52b6d3cb6254820d369a659402621a41a
Mark Shuler <biggraydog@live.com>
2016-09-04 08:13:46 -0500

    wip

--- day-34-qunit-and-object-practice/script.js
+++ day-34-qunit-and-object-practice/script.js
@@ -1,12 +1,22 @@
-'use strict';
+  'use strict';
 
 var car = new Car();
+var morePassengers;
 
 function Car(){
 }
 
-Car.prototype.honk = function(make, model) {
-  this.make = 'Toyota';
-  this.model = 'Tacoma';
+Car.prototype.honk = function() {
   return "beep beep"
+}
+
+function Car(make, model){
+  this.make = "Toyota";
+  this.model = "Tacoma";
+  this.passengers = [];
+  morePassengers = this.passengers;
+}
+
+Car.prototype.addPassengers = function(passengers){
+  morePassengers.push(passengers);
 }
